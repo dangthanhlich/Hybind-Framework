f3d4080f10752cbddcba12ab33f977c97e84477f
Dang Thi Thanh Lich <thanhlichit1999@gmail.com>
2022-06-20 14:56:19 +0700

    Allure Report - ExtentReportNG

--- actions/pageObjects/nopCommerce/user/USerHomePageObject.java
+++ actions/pageObjects/nopCommerce/user/USerHomePageObject.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 
 import commons.BasePage;
 import commons.PageGeneratorManager;
+import io.qameta.allure.Step;
 import pageUIs.nopCommerce.user.HomePageUI;
 
 public class USerHomePageObject extends BasePage {
@@ -13,45 +14,94 @@ public class USerHomePageObject extends BasePage {
 	public USerHomePageObject(WebDriver driver) {
 		this.driver = driver;
 	}
-
+	
 	// public RegisterPageObject clickToRegisterLink() {
-	// waitForElementClickable(driver, HomePageUI.REGISTER_LINK);
-	// clickToElement(driver, HomePageUI.REGISTER_LINK);
-	// return PageGeneratorManager.getRegisterPage(driver);
-	// }
-	public USerRegisterPageObject openRegisterPage() {
-		waitForElementClickable(driver, HomePageUI.REGISTER_LINK);
-		clickToElement(driver, HomePageUI.REGISTER_LINK);
-		return PageGeneratorManager.getUserRegisterPage(driver);
-	}
+		// waitForElementClickable(driver, HomePageUI.REGISTER_LINK);
+		// clickToElement(driver, HomePageUI.REGISTER_LINK);
+		// return PageGeneratorManager.getRegisterPage(driver);
+		// }
+		
+		@Step("Navigate to Register page")
+		public USerRegisterPageObject openRegisterPage() {
+			waitForElementClickable(driver, HomePageUI.REGISTER_LINK);
+			clickToElement(driver, HomePageUI.REGISTER_LINK);
+			return PageGeneratorManager.getUserRegisterPage(driver);
+		}
 
-	// public LoginPageObject clickToLoginLink() {
-	// waitForElementClickable(driver, HomePageUI.LOGIN_LINK);
-	// clickToElement(driver, HomePageUI.LOGIN_LINK);
-	// return PageGeneratorManager.getLoginPage(driver);
-	//
-	// }
-	public USerLoginPageObject openLoginPage() {
-		waitForElementClickable(driver, HomePageUI.LOGIN_LINK);
-		clickToElement(driver, HomePageUI.LOGIN_LINK);
-		return PageGeneratorManager.getUserLoginPage(driver);
+		// public LoginPageObject clickToLoginLink() {
+		// waitForElementClickable(driver, HomePageUI.LOGIN_LINK);
+		// clickToElement(driver, HomePageUI.LOGIN_LINK);
+		// return PageGeneratorManager.getLoginPage(driver);
+		//
+		// }
+		
+		@Step("Navigate to Login page")
+		public USerLoginPageObject openLoginPage() {
+			waitForElementClickable(driver, HomePageUI.LOGIN_LINK);
+			clickToElement(driver, HomePageUI.LOGIN_LINK);
+			return PageGeneratorManager.getUserLoginPage(driver);
 
-	}
+		}
 
-	public boolean isMyAccountLinkDisplayed() {
-		waitForElementVisible(driver, HomePageUI.MY_ACCOUNT_LINK);
-		return isElementDisplayed(driver, HomePageUI.MY_ACCOUNT_LINK);
-	}
+		@Step("Verify 'My Account' link to displayed")
+		public boolean isMyAccountLinkDisplayed() {
+			waitForElementVisible(driver, HomePageUI.MY_ACCOUNT_LINK);
+			return isElementDisplayed(driver, HomePageUI.MY_ACCOUNT_LINK);
+		}
 
-	// public CustomerInforPageObject clickToMyAccountLink() {
-	// waitForElementClickable(driver, HomePageUI.MY_ACCOUNT_LINK);
-	// clickToElement(driver, HomePageUI.MY_ACCOUNT_LINK);
-	// return PageGeneratorManager.getMyAccountPage(driver);
-	// }
-	public USerCustomerInforPageObject openMyAccountPage() {
-		waitForElementClickable(driver, HomePageUI.MY_ACCOUNT_LINK);
-		clickToElement(driver, HomePageUI.MY_ACCOUNT_LINK);
-		return PageGeneratorManager.getUserMyAccountPage(driver);
-	}
+		// public CustomerInforPageObject clickToMyAccountLink() {
+		// waitForElementClickable(driver, HomePageUI.MY_ACCOUNT_LINK);
+		// clickToElement(driver, HomePageUI.MY_ACCOUNT_LINK);
+		// return PageGeneratorManager.getMyAccountPage(driver);
+		// }
+		
+		@Step("Navigate to My Account page")
+		public USerCustomerInforPageObject openMyAccountPage() {
+			waitForElementClickable(driver, HomePageUI.MY_ACCOUNT_LINK);
+			clickToElement(driver, HomePageUI.MY_ACCOUNT_LINK);
+			return PageGeneratorManager.getUserMyAccountPage(driver);
+		}
+		
+		
+
+//	// public RegisterPageObject clickToRegisterLink() {
+//	// waitForElementClickable(driver, HomePageUI.REGISTER_LINK);
+//	// clickToElement(driver, HomePageUI.REGISTER_LINK);
+//	// return PageGeneratorManager.getRegisterPage(driver);
+//	// }
+//	public USerRegisterPageObject openRegisterPage() {
+//		waitForElementClickable(driver, HomePageUI.REGISTER_LINK);
+//		clickToElement(driver, HomePageUI.REGISTER_LINK);
+//		return PageGeneratorManager.getUserRegisterPage(driver);
+//	}
+//
+//	// public LoginPageObject clickToLoginLink() {
+//	// waitForElementClickable(driver, HomePageUI.LOGIN_LINK);
+//	// clickToElement(driver, HomePageUI.LOGIN_LINK);
+//	// return PageGeneratorManager.getLoginPage(driver);
+//	//
+//	// }
+//	public USerLoginPageObject openLoginPage() {
+//		waitForElementClickable(driver, HomePageUI.LOGIN_LINK);
+//		clickToElement(driver, HomePageUI.LOGIN_LINK);
+//		return PageGeneratorManager.getUserLoginPage(driver);
+//
+//	}
+//
+//	public boolean isMyAccountLinkDisplayed() {
+//		waitForElementVisible(driver, HomePageUI.MY_ACCOUNT_LINK);
+//		return isElementDisplayed(driver, HomePageUI.MY_ACCOUNT_LINK);
+//	}
+//
+//	// public CustomerInforPageObject clickToMyAccountLink() {
+//	// waitForElementClickable(driver, HomePageUI.MY_ACCOUNT_LINK);
+//	// clickToElement(driver, HomePageUI.MY_ACCOUNT_LINK);
+//	// return PageGeneratorManager.getMyAccountPage(driver);
+//	// }
+//	public USerCustomerInforPageObject openMyAccountPage() {
+//		waitForElementClickable(driver, HomePageUI.MY_ACCOUNT_LINK);
+//		clickToElement(driver, HomePageUI.MY_ACCOUNT_LINK);
+//		return PageGeneratorManager.getUserMyAccountPage(driver);
+//	}
 
 }
--- actions/pageObjects/nopCommerce/user/USerRegisterPageObject.java
+++ actions/pageObjects/nopCommerce/user/USerRegisterPageObject.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 
 import commons.BasePage;
 import commons.PageGeneratorManager;
+import io.qameta.allure.Step;
 import pageUIs.nopCommerce.user.RegisterPageUI;
 
 public class USerRegisterPageObject extends BasePage {
@@ -12,7 +13,8 @@ public class USerRegisterPageObject extends BasePage {
 	public USerRegisterPageObject(WebDriver driver) {
 		this.driver = driver;
 	}
-
+	
+	@Step("Click to register button")
 	public void clickToRegisterButton() {
 		waitForElementClickable(driver, RegisterPageUI.REGISTER_BUTTON);
 		clickToElement(driver, RegisterPageUI.REGISTER_BUTTON);
@@ -44,11 +46,13 @@ public class USerRegisterPageObject extends BasePage {
 		return getElementText(driver, RegisterPageUI.CONFIRM_PASSWORD_MESSAGE);
 	}
 
+	@Step("Enter to firstname textbox with value is {0}")
 	public void inputToFirstnameTextbox(String firstName) {
 		waitForElementVisible(driver, RegisterPageUI.FIRST_NAME_TEXTBOX);
 		sendkeyToElement(driver, RegisterPageUI.FIRST_NAME_TEXTBOX, firstName);
 	}
 
+	@Step("Enter to lastName textbox with value is {0}")
 	public void inputToLastNameTextbox(String lastName) {
 		waitForElementVisible(driver, RegisterPageUI.LAST_NAME_TEXTBOX);
 		sendkeyToElement(driver, RegisterPageUI.LAST_NAME_TEXTBOX, lastName);
@@ -60,23 +64,27 @@ public class USerRegisterPageObject extends BasePage {
 		clickToRegisterButton();
 	}
 
+	@Step("Enter to emailAddress textbox with value is {0}")
 	public void inputToEmailTextbox(String emailAddress) {
 		waitForElementVisible(driver, RegisterPageUI.EMAIL_TEXTBOX);
 		sendkeyToElement(driver, RegisterPageUI.EMAIL_TEXTBOX, emailAddress);
 	}
 
+	@Step("Enter to password textbox with value is {0}")
 	public void inputToPasswordTextbox(String password) {
 		waitForElementVisible(driver, RegisterPageUI.PASSWORD_TEXTBOX);
 		sendkeyToElement(driver, RegisterPageUI.PASSWORD_TEXTBOX, password);
 
 	}
 
+	@Step("Enter to confirmPassword textbox with value is {0}")
 	public void inputToConfirmPasswordTextbox(String confirmPassword) {
 		waitForElementVisible(driver, RegisterPageUI.CONFIRM_PASSWORD_TEXTBOX);
 		sendkeyToElement(driver, RegisterPageUI.CONFIRM_PASSWORD_TEXTBOX, confirmPassword);
 
 	}
 
+	@Step("Verify register success message is displayed")
 	public String getRegisterSuccessMessage() {
 		waitForElementVisible(driver, RegisterPageUI.REGISTER_SUCCESS_MESSAGE);
 		return getElementText(driver, RegisterPageUI.REGISTER_SUCCESS_MESSAGE);
@@ -92,5 +100,86 @@ public class USerRegisterPageObject extends BasePage {
 		waitForElementVisible(driver, RegisterPageUI.EXISTING_EMAIL_ERROR_MESSAGE);
 		return getElementText(driver, RegisterPageUI.EXISTING_EMAIL_ERROR_MESSAGE);
 	}
+
+//	public void clickToRegisterButton() {
+//		waitForElementClickable(driver, RegisterPageUI.REGISTER_BUTTON);
+//		clickToElement(driver, RegisterPageUI.REGISTER_BUTTON);
+//
+//	}
+//
+//	public String getErrorMessageAtFirstnameTextbox() {
+//		waitForElementVisible(driver, RegisterPageUI.FIRST_NAME_ERROR_MESSAGE);
+//		return getElementText(driver, RegisterPageUI.FIRST_NAME_ERROR_MESSAGE);
+//	}
+//
+//	public String getErrorMessageAtLastNameTextbox() {
+//		waitForElementVisible(driver, RegisterPageUI.LAST_NAME_ERROR_MESSAGE);
+//		return getElementText(driver, RegisterPageUI.LAST_NAME_ERROR_MESSAGE);
+//	}
+//
+//	public String getErrorMessageAtEmailTextbox() {
+//		waitForElementVisible(driver, RegisterPageUI.EMAIL_ERROR_MESSAGE);
+//		return getElementText(driver, RegisterPageUI.EMAIL_ERROR_MESSAGE);
+//	}
+//
+//	public String getErrorMessageAtPasswordTextbox() {
+//		waitForElementVisible(driver, RegisterPageUI.PASSWORD_ERROR_MESSAGE);
+//		return getElementText(driver, RegisterPageUI.PASSWORD_ERROR_MESSAGE);
+//	}
+//
+//	public String getErrorMessageAtConfirmPasswordTextbox() {
+//		waitForElementVisible(driver, RegisterPageUI.CONFIRM_PASSWORD_MESSAGE);
+//		return getElementText(driver, RegisterPageUI.CONFIRM_PASSWORD_MESSAGE);
+//	}
+//
+//	
+//	public void inputToFirstnameTextbox(String firstName) {
+//		waitForElementVisible(driver, RegisterPageUI.FIRST_NAME_TEXTBOX);
+//		sendkeyToElement(driver, RegisterPageUI.FIRST_NAME_TEXTBOX, firstName);
+//	}
+//
+//	public void inputToLastNameTextbox(String lastName) {
+//		waitForElementVisible(driver, RegisterPageUI.LAST_NAME_TEXTBOX);
+//		sendkeyToElement(driver, RegisterPageUI.LAST_NAME_TEXTBOX, lastName);
+//	}
+//
+//	public void loginToSystem(String username, String password) {
+//		inputToFirstnameTextbox(username);
+//		inputToLastNameTextbox(password);
+//		clickToRegisterButton();
+//	}
+//
+//	public void inputToEmailTextbox(String emailAddress) {
+//		waitForElementVisible(driver, RegisterPageUI.EMAIL_TEXTBOX);
+//		sendkeyToElement(driver, RegisterPageUI.EMAIL_TEXTBOX, emailAddress);
+//	}
+//
+//	public void inputToPasswordTextbox(String password) {
+//		waitForElementVisible(driver, RegisterPageUI.PASSWORD_TEXTBOX);
+//		sendkeyToElement(driver, RegisterPageUI.PASSWORD_TEXTBOX, password);
+//
+//	}
+//
+//	public void inputToConfirmPasswordTextbox(String confirmPassword) {
+//		waitForElementVisible(driver, RegisterPageUI.CONFIRM_PASSWORD_TEXTBOX);
+//		sendkeyToElement(driver, RegisterPageUI.CONFIRM_PASSWORD_TEXTBOX, confirmPassword);
+//
+//	}
+//
+//	public String getRegisterSuccessMessage() {
+//		waitForElementVisible(driver, RegisterPageUI.REGISTER_SUCCESS_MESSAGE);
+//		return getElementText(driver, RegisterPageUI.REGISTER_SUCCESS_MESSAGE);
+//	}
+//
+//	public USerHomePageObject clickToLogoutLink() {
+//		waitForElementClickable(driver, RegisterPageUI.LOGOUT_LINK);
+//		clickToElement(driver, RegisterPageUI.LOGOUT_LINK);
+//		return PageGeneratorManager.getUserHomePage(driver);
+//	}
+//
+//	public String getErrorExistingEmailMessage() {
+//		waitForElementVisible(driver, RegisterPageUI.EXISTING_EMAIL_ERROR_MESSAGE);
+//		return getElementText(driver, RegisterPageUI.EXISTING_EMAIL_ERROR_MESSAGE);
+//	}
 
 }
